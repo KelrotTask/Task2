2279c0dc72754a1c924cba254089d5ce3b27d5c4
KelrotTask <95227905+KelrotTask@users.noreply.github.com>
2021-12-09 13:02:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,22 +4,21 @@ using namespace std;
 
 int main()
 {
-    char metin[1000];			//1000 karakter alabilecek bir char array'i tanimlanmasi
-
-    cout << "sifrelenmis metni girin: ";
-    cin >> metin;			//metin input'unun alinmasi
-        for(int i = 0; i < 1000; i++)			//metindeki tüm karakterlerin
+	char metin[1000];			//1000 karakter alabilecek bir char array'i tanimlanmasi
+	cout << "sifrelenmis metni girin: ";
+	cin >> metin;			//metin input'unun alinmasi
+	for(int i = 0; i < 1000; i++)			//metindeki tÃ¼m karakterlerin
+	{  
+		if(metin[i] != char(0))			//input sonuna kadar isleme sokulmasi
 		{  
-        	if(metin[i] != char(0))			//input sonuna kadar isleme sokulmasi
-			{  
-                    metin[i] += 0;			//kodu buraya yazin
-            } 
-			else			//input sonunda islemin durdurulmasi
-			{                
-                i = 1000;
-        	}
-        }
-    cout << "gercek metin: " << metin;			//sifrelenmis metnin yazdirilmasi
-    cout << " " << endl;
-    return 0;
+		    metin[i] += 0;			//kodu buraya yazin
+		} 
+		else			//input sonunda islemin durdurulmasi
+		{                
+		    i = 1000;
+		}
+	}
+	cout << "gercek metin: " << metin;			//sifrelenmis metnin yazdirilmasi
+        cout << " " << endl;
+        return 0;
 }
